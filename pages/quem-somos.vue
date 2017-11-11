1b9c4b744ba5ba2427f4bf7575326daad40c5db1
Bruno Salgado <brunobetabio@gmail.com>
2017-11-11 19:33:14 -0200

    Quem Somos

--- pages/quem-somos.vue
+++ pages/quem-somos.vue
@@ -1,6 +1,19 @@
 <template>
-  <section>
-    <h1>quem-somos</h1>
+  <section class="container">
+    <h2>Quem Somos</h2>
+    <div class="text">
+      Vivamus suscipit tortor eget felis porttitor volutpat. Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Quisque velit nisi, pretium ut lacinia in, elementum id enim.
+      <br><br>
+      <img src="/images/cakes/1.jpg">
+      <br><br>
+      Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus.
+      <br><br>
+      Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Donec sollicitudin molestie malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.
+      <br><br>
+      Cras ultricies ligula sed magna dictum porta. Vivamus suscipit tortor eget felis porttitor volutpat. Sed porttitor lectus nibh. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Quisque velit nisi, pretium ut lacinia in, elementum id enim.
+      <br><br>
+      Vivamus suscipit tortor eget felis porttitor volutpat. Cras ultricies ligula sed magna dictum porta. Donec sollicitudin molestie malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Vivamus suscipit tortor eget felis porttitor volutpat.
+    </div>
   </section>
 </template>
 
@@ -13,3 +26,12 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.text {
+  margin-bottom: 20px;
+  img {
+    max-width: 100%;
+  }
+}
+</style>
